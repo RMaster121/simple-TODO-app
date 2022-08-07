1b527fa2c027bd9b9fdd445e357c4d3eb84a69e1
Rafał Szczerba <rs.szczerba@hotmail.com>
2022-08-07 09:03:24 +0000

    Repaired GET TodoItem list_items

--- api/models.py
+++ api/models.py
@@ -2,23 +2,25 @@ from django.db import models
 from positions.fields import PositionField
 
 # Create your models here.
+
+
 class TodoItem(models.Model):
     '''Model representing a todo item'''
     id = models.AutoField(primary_key=True)
-    type = models.CharField(max_length=1, choices=(('T', 'String'), ('L', 'List'), ('U', 'URL')), default='T')
+    type = models.CharField(max_length=1, choices=(
+        ('T', 'String'), ('L', 'List'), ('U', 'URL')))
     title = models.CharField(max_length=200, blank=False, null=False)
     due_date = models.DateField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
     text_value = models.TextField(blank=True, max_length=500)
-    url_value = models.URLField(blank=True) #TODO: add validator
+    url_value = models.URLField(blank=True)  # TODO: add validator/test it
+
 
 class ListItem(models.Model):
     '''Model representing a list item'''
     id = models.AutoField(primary_key=True)
     value = models.CharField(max_length=100)
     checked = models.BooleanField(default=False)
-    todo_item = models.ForeignKey(TodoItem, on_delete=models.CASCADE)
+    todo_item = models.ForeignKey(TodoItem, on_delete=models.CASCADE, related_name = "list_items")
     position = PositionField(default=0, unique_for_fields=('todo_item',))
-
-    
--- api/serializers.py
+++ api/serializers.py
@@ -2,47 +2,54 @@ from rest_framework import serializers
 import django_filters
 from api.models import ListItem, TodoItem
 
+
 class ListItemSerializer(serializers.ModelSerializer):
     '''Serializer for ListItem model'''
-    todo_item = serializers.PrimaryKeyRelatedField(queryset=TodoItem.objects.all(), required=False, allow_null=True, default=None)
+    todo_item = serializers.PrimaryKeyRelatedField(
+        queryset=TodoItem.objects.all(), required=False, allow_null=True, default=None)
 
-    #Although todo_item is
-    
+    # Although todo_item is
+
     class Meta:
         model = ListItem
         fields = ['id', 'value', 'position', 'todo_item']
-        
+
 
 class TodoItemSerializer(serializers.ModelSerializer):
     '''Serializer for TodoItem model'''
-    list_items = ListItemSerializer(many=True, required=False)
     created_at = django_filters.DateFromToRangeFilter()
-    
-    #Override the default create method to create a new todo item and related list items
+    list_items = ListItemSerializer(many= True, read_only = True)
+
+
+    # Override the default create method to create a new todo item and related list items
     def create(self, validated_data):
 
-        #If type is list, list_items is required.
-        #If type is string, text_value is required.
-        #If type is url, url_value is required.
-        #Else, raise an error.
-        if "type" in validated_data and validated_data["type"] == 'L':
-            if 'list_items' not in validated_data:
-                raise serializers.ValidationError('List items are required for type L.')
-        elif "type" in validated_data and validated_data["type"] == 'T':
-            if 'text_value' not in validated_data:
-                raise serializers.ValidationError('Field text_value is required for type T')
-        elif "type" in validated_data and validated_data["type"] == 'U':
-            if 'url_value' not in validated_data:
-                raise serializers.ValidationError('Field url_value is required for type U')
+        # If type is list, list_items is required.
+        # If type is string, text_value is required.
+        # If type is url, url_value is required.
+        # Else, raise an error.
+        if "type" in validated_data:
+            if validated_data["type"] == "L":
+                if "list_items" not in validated_data:
+                    raise serializers.ValidationError("List items are required for list type todo item.")
+            elif validated_data["type"] == "T":
+                if "text_value" not in validated_data:
+                    raise serializers.ValidationError("Text value is required for string type todo item.")
+            elif validated_data["type"] == "U":
+                if "url_value" not in validated_data:
+                    raise serializers.ValidationError("URL value is required for url type todo item.")
+            else:
+                raise serializers.ValidationError("Type value is incorrect.")
         else:
-            raise serializers.ValidationError('type is invalid')
+            raise serializers.ValidationError("Type is required.") 
 
         list_items_data = validated_data.pop('list_items')
         todo_item = TodoItem.objects.create(**validated_data)
 
-        #Check if type is list and if list_items is not empty
+        # Check if type is list and if list_items is not empty
         if todo_item.type == 'L' and len(list_items_data) == 0:
-            raise serializers.ValidationError('List items are required for type L.')
+            raise serializers.ValidationError(
+                'List items are required for type L.')
 
         for data in list_items_data:
             data['todo_item'] = todo_item
@@ -51,4 +58,4 @@ class TodoItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TodoItem
-        fields = '__all__'
+        fields = ('id', 'type', 'title', 'due_date', 'created_at', 'text_value', 'url_value', 'list_items', )
